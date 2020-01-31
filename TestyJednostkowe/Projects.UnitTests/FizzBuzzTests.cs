39ee0d3da3317a1e0828a618522633e72a7c379f
Krzysztof Pietrasik <43923921+KrzysztofPietrasik@users.noreply.github.com>
2020-02-01 00:35:45 +0100

    Dodany FizzBuzz i jego testy

---
+++ TestyJednostkowe/Projects.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace Projects.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        public void GetOutput_NumberIsDivisibleBy3AndOr5_ShouldReturnCorrectString(int a, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(a);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOutput_NumberIsNotDivisibleBy3Or5_ShouldReturnSameNumberAsString()
+        {
+            var result = FizzBuzz.GetOutput(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+
+        [Test]
+
+        public void GetOutput_NumberIsLesserThan0_ShouldThrowArgumentException()
+        {
+            var result = Assert.Throws<ArgumentException>(() => FizzBuzz.GetOutput(-2));
+
+            Assert.That(result.Message, Is.EqualTo("Liczba nie może być mniejsza od 0"));
+        }
+    }
+}
---
+++ TestyJednostkowe/Projects/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    public class FizzBuzz
+    {
+        public static string GetOutput(int number)
+        {
+            
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FizzBuzz";
+            
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+           
+            if (number < 0)
+                throw new System.ArgumentException("Liczba nie może być mniejsza od 0");
+
+            return number.ToString();
+        }
+    }
+}
